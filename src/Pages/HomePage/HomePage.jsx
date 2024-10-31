a6de720a20b99fe0ae38b9d8974f8344d131791d
fabroche <kraxusmmo@gmail.com>
2024-10-31 14:32:05 +0000

    feat: added content to homePage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 function HomePage(props) {
     return (
-        <h1>Home Page</h1>
+        <>
+            <h1>Home Page</h1>
+            <ol>
+                <h3>Puedes loguearte con los siguientes usuarios para ver de que forma es protegido el acceso al contenido en la sección de blogs:</h3>
+                <li>juandc</li>
+                <li>diannerd</li>
+                <li>nicobytes</li>
+                <h4>PD: Al ser con fines prácticos no se implementó la funcionalidad de los botones.</h4>
+            </ol>
+        </>
+
     );
 }
 
