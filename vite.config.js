b007754a9bc1f3c741fbbb639b59c5f8ef9cf3e7
fabroche <kraxusmmo@gmail.com>
2024-10-31 15:01:19 +0000

    chore: setup for deploy on Github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://fabroche.github.io/react-router-6/"
+  base: "https://fabroche.github.io/react-router-practice/"
 })
